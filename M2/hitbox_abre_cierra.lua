f71c88232077ba3c5c16872b627ce889ace4ce61
doniachia <chiainbox@gmail.com>
2025-09-12 10:59:29 -0500

    Create hitbox_abre_cierra.lua

---
+++ M2/hitbox_abre_cierra.lua
@@ -0,0 +1,18 @@
+local puerta = game.Workspace.etapa1.puerta
+local hitbox = script.Parent
+local boton = script.Parent.Parent:FindFirstChild("Button")
+
+local function abrir()
+	puerta.CanCollide = false
+	puerta.Transparency = 1
+	boton.BrickColor = BrickColor.Green()
+end
+
+local function cerrar()
+	puerta.CanCollide = true
+	puerta.Transparency = 0
+	boton.BrickColor = BrickColor.Red()
+end
+
+hitbox.Touched:Connect(abrir)
+hitbox.TouchEnded:Connect(cerrar)
