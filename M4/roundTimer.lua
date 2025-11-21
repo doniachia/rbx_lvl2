4597f4bc02380ab33cd4d5af7763db3161308a13
doniachia <chiainbox@gmail.com>
2025-11-21 16:10:53 -0500

    Create roundTimer.lua

---
+++ M4/roundTimer.lua
@@ -0,0 +1,9 @@
+local RoundTimer = {}
+
+RoundTimer.WaitPlayersTime = true
+RoundTimer.RestTime = 5
+RoundTimer.PreparationTime = 5
+RoundTimer.GameTime = false
+RoundTimer.WinnerTime = 5
+
+return RoundTimer
