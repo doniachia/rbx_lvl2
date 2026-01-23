b06e12f1339898acc7f071cb9fefcbf948c8eaa2
doniachia <chiainbox@gmail.com>
2026-01-23 11:14:26 -0500

    Update gui_monedas.lua
    Se añaden comentarios

--- M6/L2/gui_monedas.lua
+++ M6/L2/gui_monedas.lua
@@ -4,6 +4,7 @@ local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 
 local function createCoinGui(coin, hp)
+	-- Se crea y configura un BillboardGui con código
 	local billboardGui = Instance.new("BillboardGui")
 	billboardGui.ExtentsOffset = Vector3.new(0,2,0)
 	billboardGui.MaxDistance = 60
@@ -11,17 +12,20 @@ local function createCoinGui(coin, hp)
 	billboardGui.Size = UDim2.new(0, 150, 0, 20)
 	billboardGui.AlwaysOnTop = true
 
+	-- Se crea y configura un "Frame" dentro del anterior BillboardGui<
 	local healthBarBackground = Instance.new("Frame")
 	healthBarBackground.Parent = billboardGui
 	healthBarBackground.BackgroundColor3 = Color3.new(1, 0.917647, 0)
 	healthBarBackground.Transparency = 0.7
 	healthBarBackground.Size = UDim2.new(1, 0, 1, 0)
 
+	-- Se crea otro "Frame", dentro de "healthBackground" (copiar)
 	local healthBar = Instance.new("Frame")
 	healthBar.Parent = healthBarBackground
 	healthBar.BackgroundColor3 = Color3.new(0, 1, 0)
 	healthBar.Size = UDim2.new(1, 0, 1, 0) 
 
+	-- Se Crea un "TextLabel" dentro de "billboardGui" (copiar)
 	local textLabel = Instance.new("TextLabel")
 	textLabel.Parent = billboardGui
 	textLabel.Font = "Creepster"
